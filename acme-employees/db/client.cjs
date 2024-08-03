07e587b62372b801defb64365a79106e8b428ee6
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-03 18:13:19 -0400

    Add deployed database connection option

--- acme-employees/db/client.cjs
+++ acme-employees/db/client.cjs
@@ -1,4 +1,4 @@
 const { Client } = require('pg');
-const client = new Client('postgres://localhost:5432/acme_hr');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/acme_hr');
 
 module.exports = client;
