f0df6253fed303410a46ce5a7d451275b45e66b4
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-03 18:01:45 -0400

    Render list of employees

--- acme-employees/src/App.jsx
+++ acme-employees/src/App.jsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react"
+
 function App() {
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(()=>{
+    const getEmployees = async() => {
+      const response = await fetch('/api/v1/employees');
+      const result = await response.json();
+      setEmployees(result);
+    }
+    getEmployees();
+  },[]) 
 
   return (
     <>
-      <h1>test</h1>
+      <h1>Acme Employees</h1>
+
+      <ul>
+        { 
+          employees.map((employee) => {
+            return (
+              <>
+                <li>Name: {employee.name}</li>
+                <li>Is an administrator: {employee.is_admin.toString()}</li>
+                <br></br>
+              </>
+            )
+          })
+        }
+      </ul>
     </>
   )
 }
